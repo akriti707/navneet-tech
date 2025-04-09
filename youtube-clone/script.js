83c05b0360b352b6e3a74afed371f8db16566882
Akriti707 <akritisweta1999@gmail.com>
2025-04-09 14:21:55 +0530

    commt

---
+++ youtube-clone/script.js
@@ -0,0 +1,13 @@
+function opeen() {
+    document.getElementById("sidebar").style.width = "230px";
+    document.getElementById("container").style.marginLeft = "230px";
+}
+
+function cloce() {
+    document.getElementById("sidebar").style.width = "55px";
+    document.getElementById("container").style.marginLeft = "55px";
+}
+
+function clickactive() {
+    document.getElementsByClassName("nav-li").style.color = "red";
+}
